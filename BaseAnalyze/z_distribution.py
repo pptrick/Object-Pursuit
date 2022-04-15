6d3c686578d9bbd5374c335d8bc75d7346bd097a
Chuanyu Pan <pancy@sc.stanford.edu>
2022-04-14 19:40:14 -0700

    last commit before release

--- BaseAnalyze/z_distribution.py
+++ BaseAnalyze/z_distribution.py
@@ -1,16 +1,22 @@
 import os
 from sklearn import manifold
 import torch
+import random
 import numpy as np
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 
-label = {(0, 46):'pretrain', (47, 58):'Apple', (59, 65):'Bowl', (66, 74):'Bread', (75, 82):'Cup'}
-
-def load_z(z_dir, file_except="None"):
-    z_files = [os.path.join(z_dir, file) for file in sorted(os.listdir(z_dir)) if file.endswith(".json") and file.find(file_except)==-1]
-    zs = [torch.load(zf, map_location='cpu')['z'].numpy() for zf in z_files]
-    return zs, z_files
+def load_z(z_path, file_except="None"):
+    if os.path.isdir(z_path):
+        z_files = [os.path.join(z_path, file) for file in sorted(os.listdir(z_path)) if file.endswith(".json") and file.find(file_except)==-1]
+        zs = [torch.load(zf, map_location='cpu')['z'].numpy() for zf in z_files]
+        return zs, z_files
+    elif os.path.isfile(z_path):
+        if z_path.endswith(".pth"): # checkpoint for multinet
+            zs = torch.load(z_path, map_location='cpu')['z']
+            return zs, z_path
+    else:
+        return None, None
 
 def tSNE(zs, dim=2):
     Zs = np.stack(zs, axis=0)
@@ -19,13 +25,21 @@ def tSNE(zs, dim=2):
     return manifold.tolist()
 
 def pca(zs, dim=2):
-    Zs = np.stack(zs[75: 83], axis=0)
+    zs = np.stack(zs, axis=0)
     _pca = PCA(n_components=dim)
-    manifold = _pca.fit_transform(Zs)
+    manifold = _pca.fit_transform(zs)
     return manifold.tolist()
 
+def shuffle(zs):
+    index = [i for i in range(len(zs))]
+    random.shuffle(index)
+    return zs[index]
+
 if __name__ == "__main__":
-    zs, z_file = load_z("../Segmentation/checkpoints_objectpursuit_ithor_newtest_sequence/zs/")
+    joint_training_path = "/orion/u/pancy/project/Object-Pursuit/Segmentation/checkpoints_pretrain/checkpoints_pretrain_VOS_davis/pretrain_best.pth"
+    zs, z_file = load_z(joint_training_path)
+    # zs = shuffle(zs)
     # manifold = tSNE(zs)
     manifold = pca(zs)
     print("result: ", manifold)
+    np.save("pca_694bases_shuffle.npy", manifold)
